27379b6adfe0967365d8751d6f3400d692eac1b8
VanNguyen05 <nguyentientaivan291105@gamil.com>
2025-08-29 21:34:28 +0700

    Đẩy toàn Bộ Dự Án

---
+++ QLSV/QLSV/DangNhap.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace QLSV
+{
+    partial class DangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxMK = new System.Windows.Forms.TextBox();
+            this.checkBoxHMK = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxTK = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button3.Location = new System.Drawing.Point(499, 274);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(111, 35);
+            this.button3.TabIndex = 20;
+            this.button3.Text = "Thoát";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button2.Location = new System.Drawing.Point(345, 274);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 35);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "Đăng Ký";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(184, 274);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 35);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Đăng Nhập ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.Location = new System.Drawing.Point(454, 233);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(108, 19);
+            this.linkLabel1.TabIndex = 17;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Quên Mật Khẩu";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(168, 184);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 23);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Mật Khẩu";
+            // 
+            // textBoxMK
+            // 
+            this.textBoxMK.Location = new System.Drawing.Point(267, 181);
+            this.textBoxMK.Name = "textBoxMK";
+            this.textBoxMK.Size = new System.Drawing.Size(273, 32);
+            this.textBoxMK.TabIndex = 15;
+            this.textBoxMK.UseSystemPasswordChar = true;
+            // 
+            // checkBoxHMK
+            // 
+            this.checkBoxHMK.AutoSize = true;
+            this.checkBoxHMK.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxHMK.Location = new System.Drawing.Point(252, 232);
+            this.checkBoxHMK.Name = "checkBoxHMK";
+            this.checkBoxHMK.Size = new System.Drawing.Size(145, 23);
+            this.checkBoxHMK.TabIndex = 13;
+            this.checkBoxHMK.Text = "Hiển Thị Mật Khẩu";
+            this.checkBoxHMK.UseVisualStyleBackColor = true;
+            this.checkBoxHMK.CheckedChanged += new System.EventHandler(this.checkBoxHMK_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(164, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 23);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Tài Khoản";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(246, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(269, 31);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Đăng Nhập Hệ Thống";
+            // 
+            // textBoxTK
+            // 
+            this.textBoxTK.Location = new System.Drawing.Point(267, 138);
+            this.textBoxTK.Name = "textBoxTK";
+            this.textBoxTK.Size = new System.Drawing.Size(273, 32);
+            this.textBoxTK.TabIndex = 21;
+            // 
+            // DangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(783, 432);
+            this.Controls.Add(this.textBoxTK);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxMK);
+            this.Controls.Add(this.checkBoxHMK);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "DangNhap";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DangNhap";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxMK;
+        private System.Windows.Forms.CheckBox checkBoxHMK;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxTK;
+    }
+}
---
+++ QLSV/QLSV/DangNhap.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace QLSV
+{
+    public partial class DangNhap : Form
+    {
+        public DangNhap()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-LTL2CL3\SQLEXPRESS;Initial Catalog=QuanLySinhVien;Integrated Security=True");
+            try
+            {
+                conn.Open();
+                string tk = textBoxTK.Text;
+                string mk = textBoxMK.Text;
+                string sql = "SELECT * from DangNhap Where TenTaiKhoan = '" + tk + "'and MatKhau = '" + mk + "'";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                SqlDataReader DataReader = cmd.ExecuteReader();
+                if (textBoxMK.Text == "" || textBoxTK.Text == "")
+                {
+                    MessageBox.Show("Bạn Điền Tài Khoản Và Mật Khẩu Để Đăng Nhập","Thông Báo", MessageBoxButtons.OK,MessageBoxIcon.Question);
+                }
+                else if (DataReader.Read() == true)
+                {
+                    this.Hide();
+                    TrangChu TC = new TrangChu();
+                    TC.ShowDialog();
+                    TC = null;
+                    textBoxMK.Text = "";
+                    this.Show();
+                }
+                else
+                { 
+                    MessageBox.Show("Tài Khoản Hoặc Mật Khẩu Không Đúng? Vui Lòng Đăng Nhập Lại","Thông Báo", MessageBoxButtons.OK,MessageBoxIcon.Error);
+                    textBoxMK.Text = "";
+                }
+             }
+             catch (Exception ex)
+             {
+                MessageBox.Show("Lỗi Kết Nối"+ ex ,"Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+             }
+        }
+
+        private void checkBoxHMK_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxHMK.Checked)
+            {
+                textBoxMK.UseSystemPasswordChar = false; // Hiện Mật Khẩu
+            }
+            else 
+            {
+                textBoxMK.UseSystemPasswordChar = true; // Ẩn Mật Khẩu
+            }
+        }
+    }
+}
